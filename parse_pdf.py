b4dc32c2b2ed178d37295ccf101a6f1ec427bf6d
Shai Efrati <shaief@gmail.com>
2019-02-08 07:04:24 +0200

    Freeze latest changes

--- parse_pdf.py
+++ parse_pdf.py
@@ -11,18 +11,27 @@ def clean_double_line_breaks(text):
     return re.sub(r'\n\n', '\n', text)
 
 
-def extract_text(input, output):
-    text = textract.process(input)
+def extract_text(input_file):
+    text = textract.process(input_file)
+    # trying to use tesseract for better results:
+    # text = textract.process(input_file, method='tesseract', language='heb')
     decoded_text = text.decode()
 
     parsed_text = clean_line_numbers(decoded_text)
     parsed_text = clean_double_line_breaks(parsed_text)
 
-    with open(output, 'w') as f:
-        f.write(parsed_text)
+    return parsed_text
+
+
+def save_text_to_file(text, output_file):
+    with open(output_file, 'w') as f:
+        f.write(text)
+
+
 
 
 if __name__ == '__main__':
-    input = '/home/shaief/Downloads/9078426_14_01_2018.pdf'
-    output = input.replace('pdf', 'txt')
-    extract_text(input, output)
+    input_file = 'files/1481827_05_01_2014.pdf'
+    output_file = input_file.replace('pdf', 'txt')
+    text = extract_text(input_file)
+    save_text_to_file(text, output_file)
